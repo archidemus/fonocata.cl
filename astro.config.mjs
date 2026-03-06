f3ca03827084c1097ef637340b766a3d3c2fb3ee
Ignacio Norambuena <ignacio.norambuena@cumpl.io>
2026-03-06 18:53:55 -0300

    Formatted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fonocata.cl",
-  base: "/",
+  site: 'https://fonocata.cl',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [sitemap()],
 });
-
